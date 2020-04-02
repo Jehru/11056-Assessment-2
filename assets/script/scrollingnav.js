164128b7994eb768e5f72e89f130cd67feb92265
Jehru <jehru.e.harris@gmail.com>
2020-04-02 15:22:51 +1100

    General fixes
    Navbar scrolling, better title and hero image.

---
+++ assets/script/scrollingnav.js
@@ -0,0 +1,14 @@
+// From W3 Schools
+
+//When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navigation").style.top = "0";
+  } else {
+    document.getElementById("navigation").style.top = "-130px";
+  }
+  prevScrollpos = currentScrollPos;
+}
