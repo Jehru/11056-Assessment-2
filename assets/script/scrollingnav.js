a4ffbc6e7357c31ab2562e42b32c050b4cea64f9
Jehru <jehru.e.harris@gmail.com>
2020-05-07 21:03:41 +1000

    Thursday Night Commenting and Tidying
    Mostly adding comments, which should have been done ages ago

--- assets/script/scrollingnav.js
+++ assets/script/scrollingnav.js
@@ -1,8 +1,7 @@
-// From W3 Schools
-
 //When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar
 
 var prevScrollpos = window.pageYOffset;
+
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
