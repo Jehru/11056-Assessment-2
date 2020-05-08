9bef735eabaef35b1d2ee4007e8d42f68eebedb1
Jehru <jehru.e.harris@gmail.com>
2020-05-08 16:09:43 +1000

    Backup Before WIpe
    The media queries were not working so i wiped the code clean and tried again

--- assets/script/scrollingnav.js
+++ assets/script/scrollingnav.js
@@ -1,7 +1,11 @@
-//When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar
+// When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar
 
 var prevScrollpos = window.pageYOffset;
 
+/* When the window detects a scroll upwards move the navigation element move the navigation 
+element downwards showing it to the user
+The whole script moves the navigation off the page (-130px) when the user scrolls down
+*/
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
