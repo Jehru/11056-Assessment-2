c55a1dfd792b9fa4e118423bb92635b984dfa87d
Jehru <jehru.e.harris@gmail.com>
2020-05-15 18:01:37 +1000

    Comments

--- assets/script/scrollingnav.js
+++ assets/script/scrollingnav.js
@@ -1,10 +1,9 @@
 // When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar
 
+// Create a variable called prevScrollpos and measure the number of pixels scrolled along the Y axis
 var prevScrollpos = window.pageYOffset;
 
-/* When the window detects a scroll upwards move the navigation element move the navigation 
-element downwards showing it to the user
-The whole script moves the navigation off the page (-130px) when the user scrolls down
+/* When the previous scroll position is greater than the current position (i.e. User scrolls up) change the style of the navigation element so that it is at 0px of the top of the screen. Otherwise (User scrolls down) and change the positionig to outside the view of the user -130px
 */
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
